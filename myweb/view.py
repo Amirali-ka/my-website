3d01a96d2e3f4fcff0786e87ad54b69d95b7d317
Amirali Kavoossi <kavoossi1co@gmail.com>
2025-07-22 09:57:33 +0330

    second changes

--- myweb/view.py
+++ myweb/view.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 def index_view(request):
-    return render(request,'index.html')
+    manual_info={'name':'amirali','lastname':'kavoossi'
+                 ,'email':'kavoossi1co@gmail.com','facebook':'https://www.facebook.com/share/16aV98AZ5U/'
+                   ,'github':'https://github.com/Amirali-ka','linkedin':'http://www.linkedin.com/in/amirali-kavoossi'}
+    return render(request,'index.html',manual_info)
